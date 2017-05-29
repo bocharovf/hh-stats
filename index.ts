e345f7b8d67505a807b25e1a2bc99092ba275038
bocharovf <bocharovf@gmail.com>
2017-05-29 21:27:31 +0300

    Reexport new entities

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@
 export { HeadHunterApi } from './src/HeadHunterApi';
 export { CurrencyConverter, DictCurrencyConverter } from './src/CurrencyConverter';
 export { RequestParam } from './src/RequestParam';
+export { AreaItem } from './src/response/AreaResponse';
+export { CurrencyItem, ExperienceItem } from './src/response/DictionaryResponse';
