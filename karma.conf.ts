f6fc1d4d8146f5947e81b71d78516855d8454fbf
Philipp Bocharov <bocharovf@gmail.com>
2018-02-04 17:04:52 +0300

    Fixes Travis CI build issue with launching karma tests in Chrome

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,19 +1,31 @@
 
 export default (config:any) => {
-    config.set({
+
+    const configuration = {
         frameworks: ["jasmine", "karma-typescript"],
         files: [
             { pattern: "src/**/*.ts" },
             { pattern: "test/**/*.ts" }
         ],
-        
         preprocessors: {
-            "**/*.ts": ["karma-typescript"], // *.tsx for React Jsx 
+            "**/*.ts": ["karma-typescript"], // *.tsx for React Jsx
         },
         reporters: ["progress", "karma-typescript"],
         browsers: ["Chrome"],
         karmaTypescriptConfig: {
             tsconfig: './karma.tsconfig.json'
+        },
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
         }
-    });
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
