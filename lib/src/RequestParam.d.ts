6f95f51393e198dd5f841e4c250867ff5feee5b3
Philipp Bocharov <bocharovf@gmail.com>
2018-02-04 16:32:58 +0300

    Update js

--- lib/src/RequestParam.d.ts
+++ lib/src/RequestParam.d.ts
@@ -3,8 +3,8 @@
  */
 export declare class RequestParam {
     name: string;
-    value: string;
-    constructor(name: string, value?: string);
+    value: string | undefined;
+    constructor(name: string, value?: string | undefined);
     /**
      * Query string segment "name=val"
      */
